722b85c5d93351fa9074ac809a6c505fd395bfea
1Coder-Shivam <6868shivamkumarmaurya@gmail.com>
2024-09-29 16:53:30 +0530

    Task2 displaying lastest at top

--- script.js
+++ script.js
@@ -6,39 +6,39 @@ const todoList = document.getElementById("todo-list");
 // Load saved data from local storage
 const savedTodos = localStorage.getItem("todos");
 if (savedTodos) {
-    const todos = JSON.parse(savedTodos);
-    todos.forEach(todo => {
-        createTodoElement(todo);
-    });
+  const todos = JSON.parse(savedTodos);
+  todos.forEach((todo) => {
+    createTodoElement(todo);
+  });
 }
 
 addTodoButton.addEventListener("click", () => {
-    const title = todoTitleInput.value;
-    const description = todoDescriptionInput.value;
-    const createdAt = new Date();
+  const title = todoTitleInput.value;
+  const description = todoDescriptionInput.value;
+  const createdAt = new Date();
 
-    if (title && description) {
-        const todo = {
-            title,
-            description,
-            createdAt
-        };
+  if (title && description) {
+    const todo = {
+      title,
+      description,
+      createdAt,
+    };
 
-        createTodoElement(todo);
+    createTodoElement(todo);
 
-        // Save data to local storage
-        const todos = JSON.parse(localStorage.getItem("todos")) || [];
-        todos.push(todo);
-        localStorage.setItem("todos", JSON.stringify(todos));
+    // Save data to local storage
+    const todos = JSON.parse(localStorage.getItem("todos")) || [];
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
 
-        todoTitleInput.value = "";
-        todoDescriptionInput.value = "";
-    }
+    todoTitleInput.value = "";
+    todoDescriptionInput.value = "";
+  }
 });
 
 function createTodoElement(todo) {
-    const li = document.createElement("li");
-    li.innerHTML = `
+  const li = document.createElement("li");
+  li.innerHTML = `
         <h3>${todo.title}</h3>
         <p>${todo.description}</p>
         <p>Created at: ${todo.createdAt.toLocaleString()}</p>
@@ -48,23 +48,23 @@ function createTodoElement(todo) {
         </div>
     `;
 
-    todoList.appendChild(li);
+  todoList.prepend(li);
 
-    const deleteButton = li.querySelector(".delete-button");
-    deleteButton.addEventListener("click", () => {
-        li.remove();
+  const deleteButton = li.querySelector(".delete-button");
+  deleteButton.addEventListener("click", () => {
+    li.remove();
 
-        // Remove from local storage
-        const todos = JSON.parse(localStorage.getItem("todos"));
-        const index = todos.findIndex(t => t.createdAt === todo.createdAt);
-        todos.splice(index, 1);
-        localStorage.setItem("todos", JSON.stringify(todos));
-    });
+    // Remove from local storage
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    const index = todos.findIndex((t) => t.createdAt === todo.createdAt);
+    todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  });
 
-    const editButton = li.querySelector(".edit-button");
-    editButton.addEventListener("click", () => {
-        todoTitleInput.value = todo.title;
-        todoDescriptionInput.value = todo.description;
-        li.remove();
-    });
+  const editButton = li.querySelector(".edit-button");
+  editButton.addEventListener("click", () => {
+    todoTitleInput.value = todo.title;
+    todoDescriptionInput.value = todo.description;
+    li.remove();
+  });
 }
